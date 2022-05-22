105caecc37314d0cc720d4b46d3654cd242bf28d
FurikakeOishi <xxyanivzxx@gmail.com>
2022-05-22 21:18:24 +0300

    setup dev environment

---
+++ src/popup.jsx
@@ -0,0 +1,9 @@
+import { React } from "react";
+import { render } from "react-dom";
+import css from "popup.css";
+
+function popup(){
+
+}
+
+render(<popup/>, document.getElementById("react-target"));
