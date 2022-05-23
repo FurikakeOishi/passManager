9913378ae9277e5235287a77b7b1b8dbc7582295
FurikakeOishi <xxyanivzxx@gmail.com>
2022-05-23 22:33:47 +0300

    some basic aeasthetic

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,7 +1,13 @@
 import { React } from "react";
 import { render } from "react-dom";
-import css from "popup.css";
+import { Component } from 'react';
 
+import "./popup.css";
+$('.sub-menu ul').hide();
+$(".sub-menu a").click(function () {
+  $(this).parent(".sub-menu").children("ul").slideToggle("200");
+  $(this).find("i.fa").toggleClass("fa-angle-up fa-angle-down");
+});
 function popup(){
 
 }
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,9 @@ module.exports = {
             presets:['@babel/preset-env','@babel/preset-react'],
         }
     }
-    }]
+    },{
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],}]
   },
   plugins: [
     new HtmlWebpackPlugin({
